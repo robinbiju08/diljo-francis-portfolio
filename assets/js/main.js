0712a945633f261e4cee6598f152dc47efa2d5a6
Robin Biju <robinbiju8111@gmaili.com>
2021-02-01 23:26:50 +0530

    service section completed, portfolio design fixed, need to make seperate portfolio page for more items, portfolio contents ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,3 +135,23 @@
   });
 
 })(jQuery);
+$(document).ready(function(){
+  
+  var btnShareB = $('.btnShare2');
+  var btnShareA = $('.btnShare4');
+  
+  $('.btnShare').on('click', function(){
+    btnShareA.show();
+    btnShareB.hide();
+    $(this).children('.btnShare4').hide();
+    $(this).children('.btnShare2').css('display','flex');
+  });
+  
+  $('.btnShare5').on('click', function(){
+    event.stopPropagation();
+    btnShareA.show();
+    btnShareB.hide();
+  });
+  
+  
+});
